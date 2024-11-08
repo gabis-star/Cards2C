43f9d9e2644c8c15bc4cae3b66ee9efa83afca11
gabis-star <169148839+gabis-star@users.noreply.github.com>
2024-11-08 08:27:04 -0300

    9

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,23 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+criaCartao(
+    'Geografia',
+    'A onde fica Madagascar?',
+    'Fica na costa de Moçambique'
+)
+criaCartao(
+    'Biologia',
+    'Qual é o maior orgam do corpo humano?',
+    'O maior orgam do corpo humano é a pele'
+)
+criaCartao(
+    'Fisica',
+    'Quem descobril a radiação?',
+    'Foi Henri Becquerel em 1896'
+)
+criaCartao(
+    'Musica',
+    'Por quantas pessoas o grupo Beatles?',
+    'Por quatro jovens: John Lennon, Paul McCartney, George Harrison e Ringo Starr'
+)
